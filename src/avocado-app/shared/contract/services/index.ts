2c7a65338ea5ddb91f4e6bdf7ea4cf784145da5f
TinGoll <roma.oliynyk83@gmail.com>
2024-08-25 19:33:57 +0300

    AVCD-17: refactoring

--- src/avocado-app/shared/contract/services/index.ts
+++ src/avocado-app/shared/contract/services/index.ts
@@ -4,3 +4,4 @@ export * from "./panel-model.service.types";
 export * from "./material.service.types";
 export * from "./varnish.service.types";
 export * from "./dye.service.types";
+export * from "./orderCriticality.enum";
--- src/avocado-app/shared/contract/types/order.types.ts
+++ src/avocado-app/shared/contract/types/order.types.ts
@@ -1,18 +1,24 @@
+import { OrderCriticality } from "../services";
 import { Customer } from "../services/customer.service.types";
-import { Person } from "./person.types";
+import { Dye } from "./dye.types";
+import { FacadeModel } from "./facade-model.types";
+import { Material } from "./material.types";
+import { PanelModel } from "./panel-model.types";
+import { Patina } from "./patina.types";
+import { Varnish } from "./varnish.types";
 
 export interface Order {
   id: number;
   name: string;
-  customer: Customer;
-  author: Person;
-  priceName: string;
+  customer: Customer | null;
+  author: string;
+  priceName: string | null;
   amount: number;
   workingDays: number;
   criticality: OrderCriticality;
   createdAt: Date;
-  startedAt: Date;
-  closedAt: Date;
+  startedAt?: Date;
+  closedAt?: Date;
   documents: OrderDocument[];
 }
 
@@ -20,9 +26,34 @@ export interface OrderDocument {
   id: string;
   orderBy: number;
   name: string;
+  facadeModel: DocumentFacadeModel;
+  panelModel: DocumentPanelModel;
+  material: DocumentMaterial;
+  dye: DocumentDye;
+  varnish: DocumentVarnish;
+  patina: DocumentPatina;
 }
 
-export enum OrderCriticality {
-  NORMAL = "normal",
-  CRITICAL = "critical",
+export interface DocumentFacadeModel {
+  originalData: FacadeModel | null;
+}
+
+export interface DocumentPanelModel {
+  originalData: PanelModel | null;
+}
+
+export interface DocumentMaterial {
+  originalData: Material | null;
+}
+
+export interface DocumentDye {
+  originalData: Dye | null;
+}
+
+export interface DocumentVarnish {
+  originalData: Varnish | null;
+}
+
+export interface DocumentPatina {
+  originalData: Patina | null;
 }
