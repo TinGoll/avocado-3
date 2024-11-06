5db2069343bf080186d246db04765a4d9a64f963
TinGoll <roma.oliynyk83@gmail.com>
2024-11-06 15:05:30 +0300

    AVCD-21: update OrderField

--- src/avocado-app/features/orders/order/components/order-field/OrderField.tsx
+++ src/avocado-app/features/orders/order/components/order-field/OrderField.tsx
@@ -1,9 +1,11 @@
 import styled from "@emotion/styled";
+import { Skeleton } from "antd";
 import { FC, ReactNode } from "react";
 
 const Label = styled.span`
   font-size: 14px;
   font-weight: 400;
+  line-height: 24px;
   color: ${({ theme }) => theme.pelette.text.tertiary};
 `;
 
@@ -13,22 +15,38 @@ const Content = styled.div`
   padding-right: 12px;
   color: ${({ theme }) => theme.pelette.text.primary};
 `;
+
+const SkeletonInput = styled(Skeleton.Input)`
+  &.ant-skeleton.ant-skeleton-element {
+    & .ant-skeleton-input-sm {
+      height: 20px;
+      line-height: 20px;
+      min-width: calc(100% - 16px);
+    }
+  }
+`
 type Props = {
   label: string;
   children: ReactNode;
   labelClassName?: string;
   className?: string;
+  loading?: boolean;
 };
 export const OrderField: FC<Props> = ({
   label,
   children,
   labelClassName,
   className,
+  loading,
 }) => {
   return (
     <>
       <Label className={labelClassName}>{label}:</Label>
-      <Content className={className}>{children}</Content>
+      {loading ? (
+        <SkeletonInput size="small" active />
+      ) : (
+        <Content className={className}>{children}</Content>
+      )}
     </>
   );
 };
