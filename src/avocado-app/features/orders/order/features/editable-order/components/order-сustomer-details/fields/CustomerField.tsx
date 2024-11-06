f0be60981915169277355cb261218604012b0f51
TinGoll <roma.oliynyk83@gmail.com>
2024-11-06 15:04:28 +0300

    AVCD-21: update CustomerField

--- src/avocado-app/features/orders/order/features/editable-order/components/order-сustomer-details/fields/CustomerField.tsx
+++ src/avocado-app/features/orders/order/features/editable-order/components/order-сustomer-details/fields/CustomerField.tsx
@@ -1,39 +1,122 @@
 import { OrderField } from "@/avocado-app/features/orders/order/components/order-field";
+import { useGetCustomers } from "@/avocado-app/mocks";
+import { useCurrentOrder, useOrderId } from "@/avocado-app/shared/hooks";
 import { Editable } from "@/entities/editable";
-import { Input } from "antd";
-import { useState } from "react";
+import { Button, Select, Typography } from "antd";
+import { NOT_SELECTED } from "./defaultTextconstants";
+import { FC, useCallback, useMemo, useState } from "react";
+import { useUpdateOrder } from "@/avocado-app/mocks/order";
+import styled from "@emotion/styled";
+import { getDisplayName, searchUser } from "@/avocado-app/shared/utils";
 
-const testApi = () => {
-  return new Promise((res, rej) => {
-    setTimeout(res, 2000);
-  });
+const { Text } = Typography;
+
+const StyledNotFoundContent = styled.div`
+  text-align: center;
+  & .customer-search-text {
+    color: ${({ theme }) => theme.pelette.lime[800]};
+  }
+
+  & .customer-search-button {
+    width: 100%;
+    color: ${({ theme }) => theme.pelette.success[600]};
+  }
+`;
+
+type NotFoundContentProps = {
+  searchText?: string;
+  onClick?: () => void;
+};
+const NotFoundContent: FC<NotFoundContentProps> = ({ onClick, searchText }) => {
+  return (
+    <StyledNotFoundContent>
+      <Text type="secondary" ellipsis>
+        "<span className="customer-search-text">{searchText}</span>"<br />
+        не найден.
+      </Text>
+      <Button
+        type="default"
+        size="small"
+        className="customer-search-button"
+        onClick={onClick}
+      >
+        Добавить
+      </Button>
+    </StyledNotFoundContent>
+  );
 };
 
-export const CustomerField = () => {
-  const [data, setData] = useState<string>("Заказчик");
+export const CustomerField: FC = () => {
+  const [searchText, setSearchText] = useState<string>("");
 
-  const handleSave = async (name: string, value: string) => {
-    try {
-      await testApi();
-      setData(value);
-    } catch (error) {
-      throw error;
-    }
-  };
+  const { data: customers = [], isLoading: customersLoading } =
+    useGetCustomers();
+  const orderId = useOrderId();
+  const { data: order, isLoading: orderLoading } = useCurrentOrder();
+  const { trigger } = useUpdateOrder();
+
+  const handleSave = useCallback(
+    async (_: string, value: string) => {
+      const customer = customers.find((c) => c.login === value);
+      try {
+        await trigger({
+          id: orderId,
+          customer: customer ? { ...customer } : null,
+        });
+      } catch (error) {
+        throw error;
+      }
+    },
+    [customers]
+  );
+
+  const options = useMemo(
+    () =>
+      customers.map((customer) => ({
+        value: customer.login,
+        label: getDisplayName(customer),
+        customer: customer,
+      })),
+    [customers]
+  );
+
+  const loading = customersLoading || orderLoading || !order;
+  const customer = order?.customer;
 
   return (
-    <OrderField label="Заказчик">
+    <OrderField label="Заказчик" loading={loading}>
       <Editable<string>
         name="customer"
         confirmOnBlur
         onSave={handleSave}
-        defaultValue={data}
+        defaultValue={customer?.login || ""}
         autoSelect
-        control={({ value, ...props }) => (
-          <Input size="small" value={value} {...props} />
+        ignoredOutsideClasses={["ant-select-dropdown"]}
+        control={({ ref, ...props }) => (
+          <Select
+            style={{ minWidth: 140 }}
+            notFoundContent={
+              <NotFoundContent
+                searchText={searchText}
+                onClick={() => console.log(searchText)}
+              />
+            }
+            allowClear
+            defaultOpen
+            showSearch
+            filterOption={(inputValue, option) => {
+              return searchUser(inputValue, option?.customer);
+            }}
+            autoFocus
+            onSearch={setSearchText}
+            searchValue={searchText}
+            size="small"
+            options={options}
+            {...props}
+          />
         )}
       >
-        {data}
+        {customer ? getDisplayName(customer) : NOT_SELECTED}
       </Editable>
     </OrderField>
   );
